84e180854814b68ed0709dafb78c6d9f0c1f11ca
amnasadiqamna <amnasadiqamna05@gmail.com>
2026-06-14 15:00:23 +0500

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+include 'db.php';
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "DELETE FROM rooms WHERE id=$id";
+    if ($conn->query($sql) === TRUE) {
+        header("Location: index.php");
+        exit();
+    }
+}
+?>
